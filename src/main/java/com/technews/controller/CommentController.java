d842a80496845abfaefb50c8e6cb0a6fd8f737ac
Arjun Ghimire <ghimirearjun15@gmail.com>
2021-09-27 17:47:01 -0400

    styles, js and all the thymeleaf templated for post comment and update and delete post are created

--- src/main/java/com/technews/controller/CommentController.java
+++ src/main/java/com/technews/controller/CommentController.java
@@ -5,6 +5,9 @@ import com.technews.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
